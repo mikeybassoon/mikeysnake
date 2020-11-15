fb64e064e1ad446adbd8dbd9e9b6a37e73185b6b
Michael Macaulay <mike.macaulay@gmail.com>
2020-11-15 17:18:47 -0600

    Separate logger functions from logger module

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -13,113 +13,115 @@ module.exports = function() {
 
   var filenameTable = new Array,
 
-  // FUNCTIONS
 
-  /* function createLogFile
-
-    Parameters:
-      <1> Game ID
-  */
-  function createLogFile(gameID){
-    var timestamp = new Date(); // get current time
-    var filename = timestamp + '.txt'; // use timestamp as filename
-    try{
-      // package object
-      var record = {
-        'gameID': gameID,
-        'filename': filename
-      };
-      filenameTable.push(record);
-      fs.appendFileSync(filename, '\n');
-      console.log(`Log file created for game ${gameID}`);
-    }
-    catch(err){
-      console.log(`An error occurred while creating log file for ${gameID}:`);
-      console.log(err);
-    }
-  },
-
-  /* function log
-
-    Parameters:
-      <1> Game ID
-      <2> String to log
-
-    Appends the parameter string to the log file for this game
-  */
-
-  function log(gameID, logString){
-    var filename = filenameTable[gameID];
-    var outputString = '--' + logString + '\n';
-    try{
-      fs.appendFileSync(filename, outputString);
-    }
-    catch(err){
-      console.log(`Log operation failed.`);
-      console.log(err);
-    }
-  },
-
-  /* function logError
-
-    Parameters:
-      <1> Game ID
-      <2> String to log
-
-    Appends the parameter string to the log file for this game
-  */
-
-  function logError(gameID, logString) {
-    var filename = filenameTable[gameID];
-    var outputString = '!!ERROR: ' + logString + '\n';
-    try{
-      fs.appendFileSync(filename, outputString);
-    }
-    catch(err){
-      console.log(`Log operation failed.`);
-      console.log(err);
-    }
-  },
-
-  /* function logFunctionEntrance
-
-    Parameters:
-      <1> Game ID
-      <2> Name of function
-
-    Appends the parameter string to the log file for this game
-  */
-
-  function logFunctionEntrance(gameID, logString){
-    var filename = filenameTable[gameID];
-    var outputString = '>-' + logString + '\n';
-    try{
-      fs.appendFileSync(filename, outputString);
-    }
-    catch(err){
-      console.log(`Log operation failed.`);
-      console.log(err);
-    }
-  },
-
-  /* function logFunctionExit
-
-    Parameters:
-      <1> Game ID
-      <2> Name of function + return value if applicable
-
-    Appends the parameter string to the log file for this game
-  */
-
-  function logFunctionExit(gameID, logString) {
-    var filename = filenameTable[gameID];
-    var outputString = '<-' + logString + '\n';
-    try{
-      fs.appendFileSync(filename, outputString);
-    }
-    catch(err){
-      console.log(`Log operation failed.`);
-      console.log(err);
-    }
-  }
 };
+
+// FUNCTIONS
+
+/* function createLogFile
+
+  Parameters:
+    <1> Game ID
+*/
+function.exports createLogFile(gameID){
+  var timestamp = new Date(); // get current time
+  var filename = timestamp + '.txt'; // use timestamp as filename
+  try{
+    // package object
+    var record = {
+      'gameID': gameID,
+      'filename': filename
+    };
+    filenameTable.push(record);
+    fs.appendFileSync(filename, '\n');
+    console.log(`Log file created for game ${gameID}`);
+  }
+  catch(err){
+    console.log(`An error occurred while creating log file for ${gameID}:`);
+    console.log(err);
+  }
+},
+
+/* function log
+
+  Parameters:
+    <1> Game ID
+    <2> String to log
+
+  Appends the parameter string to the log file for this game
+*/
+
+function.exports log(gameID, logString){
+  var filename = filenameTable[gameID];
+  var outputString = '--' + logString + '\n';
+  try{
+    fs.appendFileSync(filename, outputString);
+  }
+  catch(err){
+    console.log(`Log operation failed.`);
+    console.log(err);
+  }
+},
+
+/* function logError
+
+  Parameters:
+    <1> Game ID
+    <2> String to log
+
+  Appends the parameter string to the log file for this game
+*/
+
+function.exports logError(gameID, logString) {
+  var filename = filenameTable[gameID];
+  var outputString = '!!ERROR: ' + logString + '\n';
+  try{
+    fs.appendFileSync(filename, outputString);
+  }
+  catch(err){
+    console.log(`Log operation failed.`);
+    console.log(err);
+  }
+},
+
+/* function logFunctionEntrance
+
+  Parameters:
+    <1> Game ID
+    <2> Name of function
+
+  Appends the parameter string to the log file for this game
+*/
+
+function.exports logFunctionEntrance(gameID, logString){
+  var filename = filenameTable[gameID];
+  var outputString = '>-' + logString + '\n';
+  try{
+    fs.appendFileSync(filename, outputString);
+  }
+  catch(err){
+    console.log(`Log operation failed.`);
+    console.log(err);
+  }
+},
+
+/* function logFunctionExit
+
+  Parameters:
+    <1> Game ID
+    <2> Name of function + return value if applicable
+
+  Appends the parameter string to the log file for this game
+*/
+
+function.exports logFunctionExit(gameID, logString) {
+  var filename = filenameTable[gameID];
+  var outputString = '<-' + logString + '\n';
+  try{
+    fs.appendFileSync(filename, outputString);
+  }
+  catch(err){
+    console.log(`Log operation failed.`);
+    console.log(err);
+  }
+}
